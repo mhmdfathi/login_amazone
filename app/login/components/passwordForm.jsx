03afa853fcb4f9ce39228f0e96138f4aee2bb6c7
Mohamed Fathi <50608933+mhmdfathi@users.noreply.github.com>
2026-07-28 20:02:31 +0300

    Update passwordForm.jsx

--- app/login/components/passwordForm.jsx
+++ app/login/components/passwordForm.jsx
@@ -16,7 +16,7 @@ export default function PasswordForm({
       return setErrMssgrePassword(true);
     }
 
-    await fetch("http://localhost:4000/api/user/checkPass", {
+    await fetch("http://back-production-e1fb.up.railway.app/api/user/checkPass", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
