a89c40a967b927161837b22d7a67a17b1ec8bd94
Mohamed Fathi <50608933+mhmdfathi@users.noreply.github.com>
2026-07-28 20:57:00 +0300

    Update API URL to use HTTPS

--- app/login/components/emailForm.jsx
+++ app/login/components/emailForm.jsx
@@ -18,7 +18,7 @@ export default function EmailForm({ email, setEmail, setStep }) {
     }
 
     setrLoding(true);
-    await fetch("http://back-production-e1fb.up.railway.app/api/user", {
+    await fetch("https://back-production-e1fb.up.railway.app/api/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
