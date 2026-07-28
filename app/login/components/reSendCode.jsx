2e7b3935b6047d119a635e1a8843d9a9b16ab577
Mohamed Fathi <50608933+mhmdfathi@users.noreply.github.com>
2026-07-28 20:02:51 +0300

    Update reSendCode.jsx

--- app/login/components/reSendCode.jsx
+++ app/login/components/reSendCode.jsx
@@ -33,7 +33,7 @@ export default function ReSendOtp({
     if (!time.current) return;
     reSendTime();
     if (otpType === "veriftEmail") {
-      await fetch("http://localhost:4000/api/user/reSendCode", {
+      await fetch("http://back-production-e1fb.up.railway.app/api/user/reSendCode", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -52,7 +52,7 @@ export default function ReSendOtp({
     }
 
     if (otpType === "otpPassword") {
-      await fetch("http://localhost:4000/api/user/sendOtpPassword", {
+      await fetch("http://back-production-e1fb.up.railway.app/api/user/sendOtpPassword", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
